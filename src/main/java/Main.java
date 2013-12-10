5cbcb79d30d9b7ec59de033a59a29be96a286859
Michael Lesniak <mail@mlesniak.com>
2013-12-10 06:08:12 +0100

    Moved formatting to Utils.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,4 @@
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
@@ -21,26 +13,6 @@ public class Main {
                 .execute();
 
         System.out.println(result);
-        System.out.println(prettyFormat(result, 2));
+        System.out.println(Utils.prettyFormatXML(result, 2));
     }
-
-    // http://stackoverflow.com/questions/139076/how-to-pretty-print-xml-from-java/1264912#1264912
-    public static String prettyFormat(String input, int indent) {
-        try {
-            Source xmlInput = new StreamSource(new StringReader(input));
-            StringWriter stringWriter = new StringWriter();
-            StreamResult xmlOutput = new StreamResult(stringWriter);
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            transformerFactory.setAttribute("indent-number", indent);
-            Transformer transformer = transformerFactory.newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.transform(xmlInput, xmlOutput);
-            return xmlOutput.getWriter().toString();
-        } catch (Exception e) {
-            //throw new RuntimeException(e); // simple exception handling, please review it
-            return "<exception name=\"" + e.getMessage() + "\"/>";
-        }
-    }
-
-
 }
---
+++ src/main/java/Utils.java
@@ -0,0 +1,28 @@
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+public class Utils {
+    // http://stackoverflow.com/questions/139076/how-to-pretty-print-xml-from-java/1264912#1264912
+    public static String prettyFormatXML(String input, int indent) {
+        try {
+            Source xmlInput = new StreamSource(new StringReader(input));
+            StringWriter stringWriter = new StringWriter();
+            StreamResult xmlOutput = new StreamResult(stringWriter);
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            transformerFactory.setAttribute("indent-number", indent);
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.transform(xmlInput, xmlOutput);
+            return xmlOutput.getWriter().toString();
+        } catch (Exception e) {
+            //throw new RuntimeException(e); // simple exception handling, please review it
+            return "<exception name=\"" + e.getMessage() + "\"/>";
+        }
+    }
+}
