f630a1b68476ab7aedf1205fcf237d2a40e5ab35
Michael Lesniak <mail@mlesniak.com>
2013-12-10 06:49:31 +0100

    Helper function for DOM parsing.

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,9 +1,17 @@
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 
@@ -24,5 +32,11 @@ public class Utils {
             //throw new RuntimeException(e); // simple exception handling, please review it
             return "<exception name=\"" + e.getMessage() + "\"/>";
         }
+    }
+
+    public static Document toDocument(String xml) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        return db.parse(new ByteArrayInputStream(xml.getBytes()));
     }
 }
