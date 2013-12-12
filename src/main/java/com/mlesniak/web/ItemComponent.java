3deaa7be096eba4831fdf800aa05a9d7b6caab53
Michael Lesniak <mail@mlesniak.com>
2013-12-12 07:03:26 +0100

    Display images and titles.

--- src/main/java/com/mlesniak/web/ItemComponent.java
+++ src/main/java/com/mlesniak/web/ItemComponent.java
@@ -1,7 +1,9 @@
 package com.mlesniak.web;
 
 import com.mlesniak.amazon.Item;
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.panel.Panel;
 
 public class ItemComponent extends Panel {
@@ -11,6 +13,9 @@ public class ItemComponent extends Panel {
         super(id);
         this.item = item;
 
+        Image image = new Image("image", "");
+        image.add(new AttributeModifier("src", item.getImageUrl()));
+        add(image);
         add(new Label("title", item.getTitle()));
     }
 }
