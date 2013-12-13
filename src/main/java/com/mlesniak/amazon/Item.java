8967757f1f3c4a586e4e3e4bbc3645847bf1d779
Michael Lesniak <mail@mlesniak.com>
2013-12-13 05:33:53 +0100

    Item is Serializable.

--- src/main/java/com/mlesniak/amazon/Item.java
+++ src/main/java/com/mlesniak/amazon/Item.java
@@ -1,6 +1,8 @@
 package com.mlesniak.amazon;
 
-public class Item {
+import java.io.Serializable;
+
+public class Item implements Serializable {
     private String title;
     private int price;
     private String url;
